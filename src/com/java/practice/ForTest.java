2261ed692d08358f314b2f7a6956ff5824e42e32
Jagdeep Singh Jaggi <jagdeep1691jaggi@gmail.com>
2016-09-28 04:35:04 -0400

    commit as  now coded

--- src/com/java/practice/ForTest.java
+++ src/com/java/practice/ForTest.java
@@ -1,10 +1,16 @@
 package com.java.practice;
 
-public class ForTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+{
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
 	}
-
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
+	}
 }
