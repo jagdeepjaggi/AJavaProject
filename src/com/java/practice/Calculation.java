cac1b428d778a2f4f0448007b486c2c8f73c88f8
Jagdeep Singh Jaggi <jagdeep1691jaggi@gmail.com>
2016-10-01 15:46:12 -0400

    Added subtract method to Calculation

--- src/com/java/practice/Calculation.java
+++ src/com/java/practice/Calculation.java
@@ -4,7 +4,7 @@ public class Calculation {
 public static void main(String [] args){
 	Calculation t=new Calculation();
 	System.out.println(t.add(10, 15));
-	
+	System.out.println(t.sub(30,12));
 
 }
 
@@ -12,5 +12,8 @@ public int add(int x, int y){
 	
 	return x+y;
 	
+}
+public int sub(int x, int y){
+	return x-y;
 }
 }
