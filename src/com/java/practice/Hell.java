d70fbd3cf477391225af34cbb723e3126b0d53b6
Jagdeep Singh Jaggi <jagdeep1691jaggi@gmail.com>
2016-10-01 14:57:54 -0400

    second message

--- src/com/java/practice/Hell.java
+++ src/com/java/practice/Hell.java
@@ -5,6 +5,8 @@ public class Hell {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("first message");
+
+System.out.println("second message in the class");
 	}
 
 }
